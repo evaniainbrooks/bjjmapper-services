41bd3978c356e8a4ec5d376ed6544de42e073eaa
Evan <evan@bjjmapper.com>
2016-10-29 14:50:08 +0300

    Use .env

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 ruby '2.1.1'
 
+gem 'dotenv'
+
 gemfiles = [ 'locationfetchsvc/Gemfile', 'timezonesvc/Gemfile', 'avatarsvc/Gemfile' ]
 gemfiles.each do |gemfile|
   eval_gemfile gemfile
--- application.rb
+++ application.rb
@@ -1,3 +1,10 @@
+['./avatarsvc/.env', './locationfetchsvc/.env', './timezonesvc/.env'].select {|f| File.file?(f)}.join(' ').tap do |files|
+  `cat #{files} > .env`
+end
+
+require 'dotenv'
+Dotenv.load
+
 require './locationfetchsvc/application.rb'
 require './avatarsvc/application.rb'
 require './timezonesvc/application.rb'
