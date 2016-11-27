8da92f8a1401cc3bd485c667d3ca4455d5f4fe40
Evan <evan@bjjmapper.com>
2016-11-27 21:57:01 +0200

    ln -s avatarsvc uploads dir

--- env.rb
+++ env.rb
@@ -1,5 +1,8 @@
 require 'erb'
+require 'fileutils'
 require 'dotenv'
+
+
 Dotenv.load
 
 thin_config = ERB.new(File.read("thin.yml.erb"))
@@ -7,3 +10,6 @@ File.open("thin.yml", "w") do |file|
   file.write(thin_config.result)
   file.close()
 end
+
+FileUtils.ln_s 'avatarsvc/uploads', 'uploads'
+
